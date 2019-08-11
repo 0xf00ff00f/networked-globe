dafa9b0d3fdc320327b9d369939243d6fbd9dcc2
fuse <fzorto@gmail.com>
2019-08-11 09:49:32 -0300

    hexagons

--- main.cc
+++ main.cc
@@ -17,6 +17,15 @@
 #include <algorithm>
 #include <iostream>
 #include <memory>
+#include <map>
+
+namespace glm
+{
+bool operator<(const vec3 &a, const vec3 &b)
+{
+    return std::tie(a.x, a.y, a.z) < std::tie(b.x, b.y, b.z);
+}
+}
 
 class globe_geometry_builder
 {
@@ -49,58 +58,104 @@ private:
                                                   {12, 1, 2}, {1, 12, 5},  {7, 3, 11},  {2, 7, 12}, {4, 6, 11},
                                                   {6, 5, 12}, {3, 8, 10},  {8, 2, 1},   {4, 10, 9}, {5, 9, 1}};
 
+        for (const auto &vertex : icosahedron_verts)
+        {
+            maybe_add_vertex(vertex);
+        }
+
         for (const auto &triangle : icosahedron_tris)
         {
-            const auto &v0 = icosahedron_verts[triangle[0] - 1];
-            const auto &v1 = icosahedron_verts[triangle[1] - 1];
-            const auto &v2 = icosahedron_verts[triangle[2] - 1];
-            subdivide_triangle(v0, v1, v2, 0);
+            const auto i0 = triangle[0] - 1;
+            const auto i1 = triangle[1] - 1;
+            const auto i2 = triangle[2] - 1;
+            subdivide_triangle(i0, i1, i2, 0);
         }
+
+        initialize_dual();
     }
 
-    void subdivide_triangle(const glm::vec3 &v0, const glm::vec3 &v1, const glm::vec3 &v2, int level)
+    void subdivide_triangle(int i0, int i1, int i2, int level)
     {
         if (level == max_subdivisions_)
         {
-            maybe_emit_triangle(v0, v1, v2);
+            source_verts_[i0].adjacent_triangles.push_back({i0, i1, i2});
+            source_verts_[i1].adjacent_triangles.push_back({i0, i1, i2});
+            source_verts_[i2].adjacent_triangles.push_back({i0, i1, i2});
         }
         else
         {
-            const auto v01 = glm::normalize(0.5f * (v0 + v1));
-            const auto v12 = glm::normalize(0.5f * (v1 + v2));
-            const auto v20 = glm::normalize(0.5f * (v2 + v0));
+            const auto v0 = source_verts_[i0].position;
+            const auto v1 = source_verts_[i1].position;
+            const auto v2 = source_verts_[i2].position;
 
-            subdivide_triangle(v0, v01, v20, level + 1);
-            subdivide_triangle(v01, v1, v12, level + 1);
-            subdivide_triangle(v20, v12, v2, level + 1);
-            subdivide_triangle(v01, v12, v20, level + 1);
+            const auto i01 = maybe_add_vertex(glm::normalize(0.5f * (v0 + v1)));
+            const auto i12 = maybe_add_vertex(glm::normalize(0.5f * (v1 + v2)));
+            const auto i20 = maybe_add_vertex(glm::normalize(0.5f * (v2 + v0)));
+
+            subdivide_triangle(i0, i01, i20, level + 1);
+            subdivide_triangle(i01, i1, i12, level + 1);
+            subdivide_triangle(i20, i12, i2, level + 1);
+            subdivide_triangle(i01, i12, i20, level + 1);
         }
     }
 
-    void maybe_emit_triangle(const glm::vec3 &v0, const glm::vec3 &v1, const glm::vec3 &v2)
+    int maybe_add_vertex(const glm::vec3 &v)
     {
-        const auto c0 = map_at(v0);
-        const auto c1 = map_at(v1);
-        const auto c2 = map_at(v2);
+        auto it = vert_indices_.find(v);
+        if (it != vert_indices_.end())
+            return it->second;
+        auto index = source_verts_.size();
+        source_verts_.push_back({v, {}});
+        vert_indices_.insert(it, {v, index});
+        return index;
+    }
 
-        const auto c = (c0 + c1 + c2) / 3;
+    void initialize_dual()
+    {
+        for (const auto &source_vert : source_verts_)
+        {
+            std::vector<glm::vec3> verts;
+            auto center = glm::vec3(0);
+            for (const auto &tri : source_vert.adjacent_triangles)
+            {
+                const auto vn =
+                    glm::normalize((1.0f / 3) * (source_verts_[tri.i0].position + source_verts_[tri.i1].position +
+                                                 source_verts_[tri.i2].position));
+                center += vn;
+                verts.push_back(vn);
+            }
+            for (size_t i = 1; i < verts.size() - 1; ++i)
+            {
+                auto it = std::min_element(std::next(verts.begin(), i), verts.end(),
+                                           [o = verts[i - 1]](const auto &a, const auto &b) {
+                                               return glm::length(a - o) < glm::length(b - o);
+                                           });
+                std::swap(verts[i], *it);
+            }
+            center *= (1.0f / verts.size());
+            const auto normal = glm::normalize(center);
 
-        if (c == 0.0)
-            return;
+            // flip verts if wrong orientation
+            const auto r = glm::cross(verts[0] - center, verts[1] - center);
+            if (glm::dot(r, normal) < 0)
+                std::reverse(verts.begin(), verts.end());
 
-        static constexpr float min_scale = 0.0f;
-        static constexpr float max_scale = 0.9f;
+            if (map_at(center) == 0)
+                continue;
 
-        const auto scale = min_scale + c * (max_scale - min_scale);
-        const auto center = (1.0f / 3) * (v0 + v1 + v2);
+            const float scale = 0.8f;
 
-        const auto va = center + scale * (v0 - center);
-        const auto vb = center + scale * (v1 - center);
-        const auto vc = center + scale * (v2 - center);
-
-        verts_.push_back(va);
-        verts_.push_back(vb);
-        verts_.push_back(vc);
+            for (size_t i = 0; i < verts.size(); ++i)
+            {
+                const auto &a = verts[i];
+                const auto &b = verts[(i + 1) % verts.size()];
+                const auto as = center + scale * (a - center);
+                const auto bs = center + scale * (b - center);
+                verts_.push_back({center, normal});
+                verts_.push_back({as, normal});
+                verts_.push_back({bs, normal});
+            }
+        }
     }
 
     float map_at(const glm::vec3 &v) const
@@ -118,9 +173,21 @@ private:
         return static_cast<float>(pm_.data[r * pm_.width + c]) / 255;
     }
 
+    struct triangle
+    {
+        int i0, i1, i2;
+    };
+    struct vertex_triangles
+    {
+        glm::vec3 position;
+        std::vector<triangle> adjacent_triangles;
+    };
+    std::vector<vertex_triangles> source_verts_;
+    std::map<glm::vec3, std::size_t> vert_indices_;
+
     int max_subdivisions_;
     const pixmap &pm_;
-    using vertex = std::tuple<glm::vec3>;
+    using vertex = std::tuple<glm::vec3, glm::vec3>;
     std::vector<vertex> verts_;
 };
 
@@ -144,11 +211,11 @@ public:
 private:
     void initialize_meshes()
     {
-        auto map = load_pixmap_from_png("assets/map.png");
-        if (map->type != pixmap::pixel_type::GRAY)
+        auto pm = load_pixmap_from_png("assets/map.png");
+        if (pm->type != pixmap::pixel_type::GRAY)
             panic("eh?");
-
-        globe_geometry_builder builder(5, *map);
+        std::cout << pm->width << ' ' << pm->height << '\n';
+        globe_geometry_builder builder(6, *pm);
         globe_ = builder.build_geometry();
     }
 
